9be1294d73f0aeb6d90ef85f8a120a15c7930e49
Odysseus <320873791@qq.com>
2022-09-01 12:04:57 +0800

    Added: choose input/output file with command line  argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,56 @@
+#include <unistd.h>
+
 #include "Ast.hpp"
 #include "Formatter.h"
 #include "Lexer.h"
 #include "Parser.h"
 // #include "main.h"
 
-std::fstream file;
-std::fstream outfile;
+std::fstream inputFile;
 
 void panic(const char *info) {
     std::cerr << "\nError: " << info << std::endl;
-    file.close();
-    outfile.close();
+    inputFile.close();
     std::abort();
 }
 
-int main() {
-    file.open(
-        "/Users/asterich/Desktop/MyPrograms/C++/compiler_learning/pluma_formatter/example/"
-        "test.txt");
-    if (!file.is_open()) {
+int main(int argc, char *argv[]) {
+    int opt;
+    std::string inputFileName, outputFileName;
+
+    while ((opt = getopt(argc, argv, "o:")) != -1) {
+        switch (opt) {
+            case 'o':
+                outputFileName = optarg;
+                std::cout << "-o:" << optarg << std::endl;
+                break;
+            default: /* '?' */
+                fprintf(stderr, "Usage: %s [-o output_file]  input_file\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Input file needed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    inputFileName = argv[optind];
+
+    inputFile.open(inputFileName);
+    if (!inputFile.is_open()) {
         panic("Failed to open file!\n");
     }
 
     pluma::Lexer lexer;
     std::vector<pluma::Sym> symVec;
-    while (!file.eof()) {
-        pluma::Token token = lexer.scan(file);
+    while (!inputFile.eof()) {
+        pluma::Token token = lexer.scan(inputFile);
         if (token.tokenType != pluma::TokenType::UNKNOWN) {
             symVec.push_back(pluma::Terminal{token});
         }
     }
-    file.close();
+    inputFile.close();
     symVec.push_back(pluma::Terminal{pluma::Token{"EOF", pluma::TokenType::TK_EOF}});
 
     pluma::Parser parser;
@@ -39,8 +59,7 @@ int main() {
     pluma::Ast ast = parser.grammarPtr->gen(symVec);
     ast.display();
 
-    pluma::Formatter formatter(
-        "/Users/asterich/Desktop/MyPrograms/C++/compiler_learning/pluma_formatter/example/out.txt");
+    pluma::Formatter formatter(outputFileName);
     formatter.format(ast);
 
     return 0;
